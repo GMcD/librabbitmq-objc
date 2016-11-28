9c274410c85253e3e8483d1f185686772a21217a
GMcD <gary.macdonald@projectscapa.com>
2016-11-28 10:25:51 +0000

    Extra long message buffer overwrite fix.

--- AMQPConsumer.m
+++ AMQPConsumer.m
@@ -52,24 +52,24 @@
     if ((self = [super init])) {
 		_channel = theChannel;
 		_queue = theQueue;
-		
-		amqp_basic_consume_ok_t *response = amqp_basic_consume(_channel.connection.internalConnection, 
-																_channel.internalChannel, 
-																_queue.internalQueue, 
-																AMQP_EMPTY_BYTES, 
-																!local, 
-																!ack, 
-																exclusive, 
+
+		amqp_basic_consume_ok_t *response = amqp_basic_consume(_channel.connection.internalConnection,
+																_channel.internalChannel,
+																_queue.internalQueue,
+																AMQP_EMPTY_BYTES,
+																!local,
+																!ack,
+																exclusive,
 																amqp_empty_table);
 		[_channel.connection checkLastOperation:@"Failed to start consumer" error:error];
-		
+
 		if (response == NULL){
 			return nil;
 		}
 
 		_internalConsumer = amqp_bytes_malloc_dup(response->consumer_tag);
 	}
-	
+
 	return self;
 }
 
@@ -83,66 +83,69 @@
 	amqp_frame_t frame;
 	int result = -1;
 	size_t receivedBytes = 0;
-	size_t bodySize = -1;
+    size_t bodySize = -1;
+    size_t buf = 0;
 	amqp_bytes_t body;
 	amqp_basic_deliver_t *delivery;
 	amqp_basic_properties_t *properties;
-	
+
 	AMQPMessage *message = nil;
-	
+
 	amqp_maybe_release_buffers(_channel.connection.internalConnection);
-	
+
 	while(!message) {
 		// a complete message delivery consists of at least three frames:
-		
+
 		// Frame #1: method frame with method basic.deliver
 		result = amqp_simple_wait_frame(_channel.connection.internalConnection, &frame);
 		if (result < 0) {
             return nil;
         }
-		
+
 		if (frame.frame_type != AMQP_FRAME_METHOD || frame.payload.method.id != AMQP_BASIC_DELIVER_METHOD) {
             continue;
         }
-		
+
 		delivery = (amqp_basic_deliver_t*)frame.payload.method.decoded;
-		
+
 		// Frame #2: header frame containing body size
 		result = amqp_simple_wait_frame(_channel.connection.internalConnection, &frame);
 		if (result < 0) {
             return nil;
         }
-		
+
 		if (frame.frame_type != AMQP_FRAME_HEADER) {
 			return nil;
 		}
-		
+
 		properties = (amqp_basic_properties_t*)frame.payload.properties.decoded;
-		
+
 		bodySize = (size_t)frame.payload.properties.body_size;
 		receivedBytes = 0;
 		body = amqp_bytes_malloc(bodySize);
-		
+
 		// Frame #3+: body frames
+        buf = body.bytes;
 		while(receivedBytes < bodySize) {
 			result = amqp_simple_wait_frame(_channel.connection.internalConnection, &frame);
 			if (result < 0) {
                 return nil;
             }
-			
+
 			if (frame.frame_type != AMQP_FRAME_BODY) {
 				return nil;
 			}
-			
+
 			receivedBytes += frame.payload.body_fragment.len;
-			memcpy(body.bytes, frame.payload.body_fragment.bytes, frame.payload.body_fragment.len);
+			memcpy(buf, frame.payload.body_fragment.bytes, frame.payload.body_fragment.len);
+            buf += frame.payload.body_fragment.len;
 		}
-		
+
 		message = [AMQPMessage messageFromBody:body withDeliveryProperties:delivery withMessageProperties:properties receivedAt:[NSDate date]];
-		
+
 		amqp_bytes_free(body);
 	}
-	
+
 	return message;
 }
 
